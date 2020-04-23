92a4b95048dcfeece1345b001b4a8582d2edd7aa
mdziardziel <mdziardziel@gmail.com>
2020-04-23 18:45:12 +0200

    seed products

--- app/models/product.rb
+++ app/models/product.rb
@@ -1,12 +1,12 @@
 class Product < ApplicationRecord
-  include Discard::Model
+  include ::Discard::Model
 
   DEFAULT_PICTURE = 'https://upload.wikimedia.org/wikipedia/en/0/00/The_Child_aka_Baby_Yoda_%28Star_Wars%29.jpg'
   PICTURE_SIZE = '250x250'
 
-  validate :name, :description, :quantity, :price, presence: true
-  validate :quantity, numericality: true
-  validate :price, numericality: { only_integer: true }
+  validates :name, :description, :quantity, :price, presence: true
+  validates :price, numericality: true
+  validates :quantity, numericality: { only_integer: true }
 
   def picture_url
     picture || DEFAULT_PICTURE
--- spec/factories/product_factory.rb
+++ spec/factories/product_factory.rb
@@ -3,7 +3,7 @@ FactoryBot.define do
     name { Faker::Commerce.product_name }
     description { Faker::Lorem.paragraph }
     picture { Faker::Avatar.image(size: Product::PICTURE_SIZE) }
-    quantity { Faker::Number.positive }
+    quantity { Faker::Number.positive.to_i }
     price { Faker::Commerce.price }
   end
 end
