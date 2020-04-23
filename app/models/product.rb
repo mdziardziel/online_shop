6d5a59eb8b23051a6b0c839bbdbf5495771a0bdc
mdziardziel <mdziardziel@gmail.com>
2020-04-23 18:21:02 +0200

    validate product

--- app/models/product.rb
+++ app/models/product.rb
@@ -1,2 +1,13 @@
 class Product < ApplicationRecord
+  include Discard::Model
+
+  DEFAULT_PICTURE = 'https://upload.wikimedia.org/wikipedia/en/0/00/The_Child_aka_Baby_Yoda_%28Star_Wars%29.jpg'
+
+  validate :name, :description, :quantity, :price, presence: true
+  validate :quantity, numericality: true
+  validate :price, numericality: { only_integer: true }
+
+  def picture_url
+    picture || DEFAULT_PICTURE
+  end
 end
