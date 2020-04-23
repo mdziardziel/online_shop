0a54b890565454e43bd2585ad037fafa1b9e14a0
mdziardziel <mdziardziel@gmail.com>
2020-04-23 19:14:29 +0200

    add acitve admin

--- db/seeds.rb
+++ db/seeds.rb
@@ -6,4 +6,4 @@
 #   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
 #   Character.create(name: 'Luke', movie: movies.first)
 
-FactoryBot.create_list(:product, 15)
+FactoryBot.create_list(:product, 15)AdminUser.create!(email: 'admin@example.com', password: 'password', password_confirmation: 'password') if Rails.env.development?
