3385a90c38006427157d494f3d1cb5f6265f3c21
mdziardziel <mdziardziel@gmail.com>
2020-04-23 18:20:39 +0200

    discard gem

--- app/controllers/products_controller.rb
+++ app/controllers/products_controller.rb
@@ -4,7 +4,7 @@ class ProductsController < ApplicationController
   # GET /products
   # GET /products.json
   def index
-    @products = Product.all
+    @products = Product.kept
   end
 
   # GET /products/1
