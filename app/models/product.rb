da18cb274236fd1a739bb0f742e635a3a4100045
mdziardziel <mdziardziel@gmail.com>
2020-04-23 18:40:50 +0200

    add faker

--- app/models/product.rb
+++ app/models/product.rb
@@ -2,6 +2,7 @@ class Product < ApplicationRecord
   include Discard::Model
 
   DEFAULT_PICTURE = 'https://upload.wikimedia.org/wikipedia/en/0/00/The_Child_aka_Baby_Yoda_%28Star_Wars%29.jpg'
+  PICTURE_SIZE = '250x250'
 
   validate :name, :description, :quantity, :price, presence: true
   validate :quantity, numericality: true
--- spec/factories/product_factory.rb
+++ spec/factories/product_factory.rb
@@ -1,8 +1,9 @@
 FactoryBot.define do
   factory :product do
-    name {}
-    description {}
-    picture {}
-    quantity {}
+    name { Faker::Commerce.product_name }
+    description { Faker::Lorem.paragraph }
+    picture { Faker::Avatar.image(size: Product::PICTURE_SIZE) }
+    quantity { Faker::Number.positive }
+    price { Faker::Commerce.price }
   end
 end
