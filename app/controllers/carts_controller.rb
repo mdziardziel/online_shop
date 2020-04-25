f7755591cdd616ea05f3175c65df876b340f66aa
mdziardziel <mdziardziel@gmail.com>
2020-04-25 23:30:10 +0200

    change theme

--- app/controllers/carts_controller.rb
+++ app/controllers/carts_controller.rb
@@ -8,7 +8,7 @@ class CartsController < ApplicationController
 
   def cart
     cart_hash = JSON.parse(cookies['cart'].presence || '{}')
-    cart_tmp = cart_hash.map { |id, quantity| [Product.find(id), quantity] }
+    cart_tmp = cart_hash.map { |id, quantity| [Product.find(id), quantity.to_i] }
     @cart ||= cart_tmp.each_with_object([]) { |cart, arr| arr <<  cart if cart.second > 0 }
   end
 end
