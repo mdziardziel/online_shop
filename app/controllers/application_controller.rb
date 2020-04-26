09d8a24f6ff86ace4c90cb7121ff9544a9666767
mdziardziel <mdziardziel@gmail.com>
2020-04-26 10:31:33 +0200

    disable basic auth for provider endpoint

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,5 +1,5 @@
 class ApplicationController < ActionController::Base
-  http_basic_authenticate_with name: ENV['BASIC_AUTH_LOGIN'], password: ENV['BASIC_AUTH_PASSWORD']
+  http_basic_authenticate_with name: ENV['BASIC_AUTH_LOGIN'], password: ENV['BASIC_AUTH_PASSWORD'], unless: :basic_auth_disabled
 
   helper_method :categories
 
@@ -7,5 +7,9 @@ class ApplicationController < ActionController::Base
   
   def categories
     Product.kept.pluck(:category).uniq
+  end
+
+  def basic_auth_disabled
+    controller_name == 'payments' && action_name == 'provider_notify'
   end
 end
